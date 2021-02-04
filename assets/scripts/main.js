974e4837c0182b7517d9cdf6474074656a470ede
Annika Hatcher <anhatche@ucsd.edu>
2021-02-03 21:24:09 -0800

    update volume image

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,13 +6,19 @@ airHorn = document.getElementById("radio-air-horn");
 carHorn = document.getElementById("radio-car-horn");
 partyHorn = document.getElementById("radio-party-horn");
 
-// Horn sound/image
+// Horn sound/images
 hornSound = document.getElementById("horn-sound");
 hornImage = document.getElementById("sound-image");
 
 // Volume inputs
 volumeSlider = document.getElementById("volume-slider");
 volumeNumber = document.getElementById("volume-number");
+
+// Volume image
+volumeImage = document.getElementById("volume-image");
+
+// Honk button
+honkButton = document.getElementById("honk-btn");
 
 // Update audio and image based on selection
 airHorn.onclick = function() {
@@ -31,10 +37,46 @@ partyHorn.onclick = function() {
     hornImage.alt = "Party Horn";
 }
 
-// Sync volume slider and number input
+// Sync volume slider and number input, update volume image
 volumeSlider.oninput = function() {
-    volumeNumber.value = volumeSlider.value;
+    var volume = volumeSlider.value;
+    volumeNumber.value = volume;
+
+    if (volume == 0) {
+        honkButton.disabled = true;
+        volumeImage.src = "./assets/media/icons/volume-level-0.svg";
+    }
+    else {
+        honkButton.disabled = false;
+
+        if (volume >= 67) {
+            volumeImage.src = "./assets/media/icons/volume-level-3.svg";
+        }
+        else if (volume >= 34) {
+            volumeImage.src = "./assets/media/icons/volume-level-2.svg";
+        }
+        else {
+            volumeImage.src = "./assets/media/icons/volume-level-1.svg";
+        }
+    }
 }
 volumeNumber.oninput = function() {
-    volumeSlider.value = volumeNumber.value;
+    var volume = volumeNumber.value;
+    volumeSlider.value = volume;
+    if (volume == 0) {
+        honkButton.disabled = true;
+    }
+    else {
+        honkButton.disabled = false;
+
+        if (volume >= 67) {
+            volumeImage.src = "./assets/media/icons/volume-level-3.svg";
+        }
+        else if (volume >= 34) {
+            volumeImage.src = "./assets/media/icons/volume-level-2.svg";
+        }
+        else {
+            volumeImage.src = "./assets/media/icons/volume-level-1.svg";
+        }
+    }
 }
